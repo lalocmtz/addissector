ea089271eef4a708643b1e2e52f5fdf01ab9bdef
lalocmtz <lalocmtz@gmail.com>
2026-07-28 20:21:01 +0800

    Update page.tsx

--- src/app/app/cerebro/page.tsx
+++ src/app/app/cerebro/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
 export default function CerebroPage() {
-  return <iframe src="/cerebro-marca.html" title="Cerebro de la marca" style={{ width: '100%', height: 'calc(100vh - 57px)', border: 0, background: '#0A0D14' }} />;
+  return <iframe src="/cerebro-os.html" title="Cerebro de la marca" style={{ width: '100%', height: 'calc(100vh - 57px)', border: 0, background: '#0A0D14' }} />;
 }
