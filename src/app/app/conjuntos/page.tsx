d04b3ddc8863fc64e39be76579124b2765a88c63
lalocmtz <lalocmtz@gmail.com>
2026-07-28 20:23:32 +0800

    Update page.tsx

--- src/app/app/conjuntos/page.tsx
+++ src/app/app/conjuntos/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
 export default function ConjuntosPage() {
-  return <iframe src="/studio-lab.html" title="Analisis de Meta" style={{ width: '100%', height: 'calc(100vh - 57px)', border: 0, background: '#F7F8FA' }} />;
+  return <iframe src="/analisis-meta.html" title="Analisis de Meta" style={{ width: '100%', height: 'calc(100vh - 57px)', border: 0, background: '#0A0D14' }} />;
 }
